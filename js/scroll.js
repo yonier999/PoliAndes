d43bf970c7d22d63230d6f782e1ac44b90aaffa8
yonier999 <yonierjulian999@gmail.com>
2021-02-05 09:34:13 -0500

    interactividad de cambio de tamaño con scroll en responsive

--- js/scroll.js
+++ js/scroll.js
@@ -1,19 +1,57 @@
-// avento de scroll
-$(document).ready(function() {
-    let img = document.getElementById('imagen_poliandes')
-    let header = document.getElementById('header-nav')
+function isMobile() {
+    return (
+        (navigator.userAgent.match(/Android/i)) ||
+        (navigator.userAgent.match(/webOS/i)) ||
+        (navigator.userAgent.match(/iPhone/i)) ||
+        (navigator.userAgent.match(/iPod/i)) ||
+        (navigator.userAgent.match(/iPad/i)) ||
+        (navigator.userAgent.match(/BlackBerry/i))
+    );
+}
 
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 0) {
-            img.style.width = '120px'
-            img.style.marginLeft = '47%'
-            header.style.background = 'white'
-        } else {
-            img.style.width = '22.4375rem'
-            img.style.marginLeft = '-2%'
-            header.style.background = 'none'
+if (isMobile()) {
+    // avento de scroll
+    $(document).ready(function() {
+        let img = document.getElementById('imagen_poliandes');
+        let header = document.getElementById('header-nav');
 
-        }
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 0) {
+                img.style.width = '11.4375rem';
+                // img.style.width = '120px';
+                // img.style.marginLeft = '47%';
+                img.style.marginLeft = '-2%';
+
+                header.style.background = 'white';
+            } else {
+                img.style.width = '11.4375rem';
+                img.style.marginLeft = '-2%';
+                header.style.background = 'none';
+
+            }
+        });
+
+    });
+} else {
+    // avento de scroll
+    $(document).ready(function() {
+        let img = document.getElementById('imagen_poliandes');
+        let header = document.getElementById('header-nav');
+
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 0) {
+                img.style.width = '120px';
+                // img.style.width = '10.4375rem';
+                img.style.marginLeft = '47%';
+                header.style.background = 'white';
+            } else {
+                img.style.width = '22.4375rem';
+                img.style.marginLeft = '-2%';
+                header.style.background = 'none';
+
+            }
+        });
+
     });
 
-});
+}
